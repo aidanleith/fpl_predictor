82a11e8ab83f10259fa8f720749f1e0be4f385a4
Aidan Leith <aidanleith5@gmail.com>
2025-07-25 23:24:06 -0400

    Basically finished data preprocessing, missing handling null values.

--- ml/preprocessing/featureEngineering.py
+++ ml/preprocessing/featureEngineering.py
@@ -13,10 +13,11 @@ class featureEngineer:
         self.position = position
         self.season = season
 
-    #creates 3 to 5 game rolling averages of points, goals, assists, and mins
+    #creates 3 to 5 game rolling averages for features that all players regardless of position gain/lose points from
     def createRollingAverages(self, df):
-        rollingCols = ["total_points", "goals_scored", "assists", "minutes"]
-
+        rollingCols = ["total_points", "goals_scored", "assists", "minutes",
+                       "expected_goals", "expected_goal_involvements", "expected_assists",
+                       "yellow_cards", "red_cards", "bonus"]
         #last 3 averages, requires at least 1 previous gameweek, first gameweek will always be None
         for col in rollingCols:
             df[f'{col}_last3'] = (
@@ -35,12 +36,43 @@ class featureEngineer:
                     )
         return df
     
-    def createFormIndicators(self, df):
-
-        return df
-    
+    #position specific features. note: FWD have no specific feature to gain or lose points from outside of general features
     def createPositionalStats(self, df):
+        if self.position == "GK":
+            newCols = ['saves', 'clean_sheets', 'goals_conceded', 'penalties_saved', 
+                       'expected_goals_conceded']
+            
+            for col in newCols:
+                df[f'{col}_last3'] = (
+                        df[f'{col}']
+                        .shift(1)
+                        .rolling(3, min_periods=1)
+                        .mean()
+                )
 
+
+        elif self.position == "DEF":
+            newCols = ['goals_conceded', 'expected_goals_conceded', 'clean_sheets']
+            
+            for col in newCols:
+                df[f'{col}_last3'] = (
+                        df[f'{col}']
+                        .shift(1)
+                        .rolling(3, min_periods=1)
+                        .mean()
+                )
+
+        #midfield specific stats, dont lose points for conceding but gain 1 for clean sheet
+        elif self.position == "MID":
+            newCols = ['clean_sheets']
+
+            for col in newCols:
+                df[f'{col}_last3'] = (
+                        df[f'{col}']
+                        .shift(1)
+                        .rolling(3, min_periods=1)
+                        .mean()
+                )
         return df
     
     #encode categorical variables
@@ -48,9 +80,14 @@ class featureEngineer:
         df['was_home'] = df['was_home'].astype(int)
         return df
     
+    #add opponent team strength for each gameweek from team data csv
     def addOpponentStrength(self, df):
         collect = dataCollector()
         teamDf = collect.teamData(self.season)
+        df = df.merge(teamDf[['strength', 'id']], left_on='opponent_team', right_on='id', how='left')
+        df.drop(columns='id', inplace=True)
+
+        return df
         
     
     #ensure no gw repeats as there are in csv file
@@ -64,8 +101,30 @@ class featureEngineer:
 
         return df
 
+    #returns x,y with x being training features and y being total points in dataframes
     def finalDf(self,df):
-        colsToKeep = ["was_home", "value", "selected", "transfers_in", "transfers_out"]
+        colsToKeep = ["was_home", "value", "selected", "transfers_in", "transfers_out",
+                      "total_points_last3", "total_points_last5", "goals_scored_last3", 
+                      "goals_scored_last5", "assists_last3", "assists_last5", "minutes_last3",
+                      "minutes_last5", "expected_goals_last3", "expected_goals_last5",
+                      "expected_goal_involvements_last3", "expected_goal_involvements_last5",
+                      "expected_assists_last3", "expected_assists_last5", "yellow_cards_last3", 
+                      "yellow_cards_last5", "red_cards_last3", "red_cards_last5", "bonus_last3",
+                      "bonus_last5", "strength"]
+        
+        positionSpecific = []
+        
+        #use .extend to pass through multiple arguments, append accepts only 1
+        if self.position == "GK":
+            positionSpecific.extend(['saves_last3', 'clean_sheets_last3', 'goals_conceded_last3',
+                                 'penalties_saved_last3', 'expected_goals_conceded_last3'])
+        elif self.position == "DEF":
+            positionSpecific.extend(['goals_conceded_last3', 'expected_goals_conceded_last3', 'clean_sheets_last3'])
+        elif self.position == "MID":
+            positionSpecific.extend(["clean_sheets_last3"])
+
+        for col in positionSpecific:
+            colsToKeep.append(col)
 
         x = df[colsToKeep]
         y = df["total_points"]
--- ml/preprocessing/pipeline.py
+++ ml/preprocessing/pipeline.py
@@ -3,18 +3,24 @@ from ml.preprocessing.dataCleaning import cleanData
 from ml.preprocessing.featureEngineering import featureEngineer
 from ml.dataCollection.collector import dataCollector
 
+'''
+The following class retrieves the original data for an individual player for a specific season and 
+prepares the dataframe for training. Data cleaning and feature engineering results 2 final dataframes,
+x and y.
+'''
 class MLPipeline:
 
     def __init__(self, position, season):
         self.cleaner = cleanData()
         self.engineer = featureEngineer(position, season)
         self.collect = dataCollector()
-        self.position = position
 
+    #retrieve raw data on a player for a specific season
     def retrieveData(self, season):
         df = self.collect.gameweekMerged(season)
         return df
 
+    #data cleaning and feature engineering. returns my training features (x) and target values (y)
     def preprocessData(self, df):
         dfClean = self.cleaner.cleanAndSort(df)
 
@@ -22,14 +28,17 @@ class MLPipeline:
         dfEngineered = self.engineer.encode(dfEngineered)
         dfEngineered = self.engineer.createRollingAverages(dfEngineered)
         dfEngineered = self.engineer.addOpponentStrength(dfEngineered)
-        dfEngineered = self.engineer.createFormIndicators(dfEngineered)
-        finalDf = self.engineer.finalDf(dfEngineered)
+        dfEngineered = self.engineer.createPositionalStats(dfEngineered)
+        x, y = self.engineer.finalDf(dfEngineered)
+
+        print(x.columns)
 
 
 p = MLPipeline("GK", "2024-25")
 df = p.retrieveData("2024-25")
-df = df[df["element"] == 4]
+df = df[df["element"] == 344]
 p.preprocessData(df)
+
     
 
     
